e07b4e103d2ee46e6c0de9be4cd3094fe2f35704
soumyabiswas250498 <soumyabiswas250498@gmail.com>
2024-01-06 15:04:03 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className='text-yellow-300'>
+      <h1 className='bg-slate-400'>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
